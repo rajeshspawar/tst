acf4bb62b77ecc873e2d1cf141cc8a06ec2bded2
rajeshspawar <rajeshspawar@yahoo.com>
2025-08-15 17:30:05 +0530

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js')
-    .then(() => console.log('Service Worker Registered'))
-    .catch(error => console.error('Service Worker Error:', error));
-}
